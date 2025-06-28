f464f816a510ac10a3c514b1f92e9d4619461598
Ryan Anderson <ryan@michonline.com>
2025-06-27 17:48:03 -0700

    Add in KC_PAUSE on FN + "." to provide `brightness up` on MacOS
    Scroll lock is `brightness down` and is also used by kvm switching, so
    this lets me fix that.

--- keyboards/keyboardio/model01/keymaps/pugmajere/keymap.c
+++ keyboards/keyboardio/model01/keymaps/pugmajere/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
   KC_TAB , _______, KC_MS_U, KC_WH_U, KC_BTN3, MACRO_KVM_1, RGB_TOG,    KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_F12 ,
   KC_HOME, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, MACRO_KVM_2, RGB_SPI,    KC_MPLY, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
-  KC_END , KC_PSCR, KC_INS , KC_WH_D, KC_BTN2, MACRO_KVM_4, RGB_SPD,    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_BSLS, KC_PIPE,
+  KC_END , KC_PSCR, KC_INS , KC_WH_D, KC_BTN2, MACRO_KVM_4, RGB_SPD,    MACRO_KVM_3, KC_MUTE, KC_VOLD, KC_VOLU, KC_PAUS, KC_BSLS, KC_PIPE,
                                          _______,                                 _______,
                                             KC_DEL ,                           KC_ENT ,
                                                _______,                     _______,
